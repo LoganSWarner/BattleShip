553e085e6c1d3901f55e6b5b81a0b3fa254f917c
Logan Warner <loganswarnerprofessional@gmail.com>
2017-04-18 16:05:28 -0600

    Initial creation of battle page

---
+++ battle.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+  require 'lab-2017-04-04/ArenaController.php';
+  $arena_controller = new ArenaController();
+
+  if(!isset($_SESSION['Started'])) {
+    echo "AVAILABLE\r\n";
+    $us_first = rand(0, 1);
+    $_SESSION['us_first'] = $us_first;
+    echo 'ROLL:'.$us_first."\r\n";
+    $hash = hash('MD5', $_SERVER['REMOTE_ADDR'].$_SERVER['REQUEST_TIME'].$_SERVER[['REQUEST_TIME']);
+    echo 'SESSION:'.$hash."\r\n";
+    $_SESSION['Started'] = True;
+  } else
+    echo "UNAVAILABLE\r\n";
+?>
