12b5c9fbc05f4000e05e131a57a3ef0d38852faa
Logan Warner <loganswarnerprofessional@gmail.com>
2017-04-18 19:43:48 -0600

    Fix syntax in battle

--- battle.php
+++ battle.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 require 'ArenaController.php';
-$arena_controller = new ArenaController()
+$arena_controller = new ArenaController();
 
 if(!isset($_SESSION['Started'])) {
   echo "AVAILABLE\r\n";
   $us_first = rand(0, 1);
   $_SESSION['us_first'] = $us_first;
   echo 'ROLL:'.$us_first."\r\n";
-  $hash = hash('MD5', $_SERVER['REMOTE_ADDR'].$_SERVER['REQUEST_TIME'].$_SERVER[['REQUEST_TIME']);
+  $hash = hash('md5', $_SERVER['REMOTE_ADDR'].$_SERVER['REQUEST_TIME'].$_SERVER['REQUEST_TIME']);
   echo 'SESSION:'.$hash."\r\n";
   $_SESSION['Started'] = True;
 } else
